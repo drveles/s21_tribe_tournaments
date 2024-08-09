9cf434140b53f462c9a2c4200807c1a520d95b04
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-09 21:45:12 +0300

    add: simple style.css added and connected to html

--- django_app/main/s21_tribe_tournaments/urls.py
+++ django_app/main/s21_tribe_tournaments/urls.py
@@ -3,5 +3,5 @@ from s21_tribe_tournaments import views
 
 urlpatterns = [
     path('', views.main_page),
-    re_path(r'^(?P<re_str>[a-z]{3})/', views.tribe),
+    re_path(r'^(?P<re_str>[a-z]{3})/$', views.tribe),
 ]
