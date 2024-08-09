5c251c670b54368777b874584d7c6e2d287fdee0
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-09 19:09:25 +0300

    add: base.html extends to all pages

--- django_app/main/s21_tribe_tournaments/views.py
+++ django_app/main/s21_tribe_tournaments/views.py
@@ -5,15 +5,25 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    data = {"items" : ["A это", "list", 2, {1, 3}]}
+    data = {
+        "title": "Tribe tournaments",
+        "items": ["A это", "list", 2, {1, 3}],
+    }
     return render(request, "tournaments/index.html", data)
 
 
 def tribe(request, re_str):
     if re_str not in ("kzn", "msk"):
         raise Http404()
-    return render(request, "tournaments/campuses.html", {"re_str": re_str})
+    data = {
+        "title": f"{re_str.upper()} campus",
+        "re_str": re_str,
+    }
+    return render(request, "tournaments/campuses.html", data)
 
 
 def page_not_found(request, exception):
-    return render(request, "tournaments/error404.html")
+    data = {
+        "title": "404!"
+    }
+    return render(request, "tournaments/error404.html", data)
