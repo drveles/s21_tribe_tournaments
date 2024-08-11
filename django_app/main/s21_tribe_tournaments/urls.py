3f97769d786771dae032ef0dc1a14932cce0d0af
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-11 17:49:52 +0300

    add: correct url to logo

--- django_app/main/s21_tribe_tournaments/urls.py
+++ django_app/main/s21_tribe_tournaments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from s21_tribe_tournaments import views
 
 urlpatterns = [
-    path('', views.main_page),
-    re_path(r'^(?P<re_str>[a-z]{3})/$', views.tribe),
+    path('', views.main_page, name="tournaments_main"),
+    re_path(r'^(?P<re_str>[a-z]{3})/$', views.tribe, name="campus_page"),
 ]
