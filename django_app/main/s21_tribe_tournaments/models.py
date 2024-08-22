d60352799c79a54867ca3a4068e8fe4fafbf8e67
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-22 20:14:05 +0300

    add: magic __str__ to all models

--- django_app/main/s21_tribe_tournaments/models.py
+++ django_app/main/s21_tribe_tournaments/models.py
@@ -7,11 +7,17 @@ class Campuses(models.Model):
     name = models.CharField(max_length=52)
     slug = models.CharField(max_length=52)
 
+    def __str__(self):
+        return self.name
+
 
 class Tournaments(models.Model):
     name = models.CharField(max_length=128)
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 
 class Tribes(models.Model):
@@ -25,14 +31,27 @@ class Tribes(models.Model):
     curr_points = models.IntegerField(default=0)
     prev_points = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class Peers(models.Model):
     name = models.CharField(max_length=21)
     campus_id = models.ForeignKey(Campuses, on_delete=models.CASCADE)
-    curr_tribe_id = models.ForeignKey(Tribes, on_delete=models.CASCADE, related_name="curr_tribe_id")
-    prev_tribe_id = models.ForeignKey(Tribes, on_delete=models.CASCADE, related_name="prev_tribe_id",
-                                      null=True, default=None)
+    curr_tribe_id = models.ForeignKey(
+        Tribes, on_delete=models.CASCADE, related_name="curr_tribe_id"
+    )
+    prev_tribe_id = models.ForeignKey(
+        Tribes,
+        on_delete=models.CASCADE,
+        related_name="prev_tribe_id",
+        null=True,
+        default=None,
+    )
     level = models.IntegerField()
     wave = models.CharField(max_length=21)
     curr_points = models.IntegerField(default=0)
     prev_points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
