5b65c21d5dc0168c1a1412decd6cb9d387e4f33d
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-23 20:54:30 +0300

    upd: mvp campus_page ready

--- django_app/main/s21_tribe_tournaments/static/tournaments/js/script.js
+++ django_app/main/s21_tribe_tournaments/static/tournaments/js/script.js
@@ -1,11 +1,15 @@
-//window.addEventListener("scroll", function() {
-//    var header = document.querySelector(".header-wrapper");
-//    var logo = document.querySelector(".header-logo");
-//    var headerHeight = header.offsetHeight;
-//
-//    if (window.pageYOffset > headerHeight) {
-//        logo.classList.add("floating");
-//    } else {
-//        logo.classList.remove("floating");
-//    }
-//});
+function toggleBlock(event) {
+    var clickedElement = event.currentTarget;
+    var nextElement = clickedElement.nextElementSibling;
+
+    if (nextElement) {
+        nextElement.classList.toggle("__hidden");
+    }
+
+    // var tribes = document.getElementsByClassName("tribe-info");
+
+    // for (var i = 0; i < tribes.length; i++) {
+    //     console.log(tribes[i])
+    //     tribes[i].nextElementSibling.classList.add("__hidden");
+    // }
+}
