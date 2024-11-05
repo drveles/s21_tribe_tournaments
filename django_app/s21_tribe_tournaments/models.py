0f91282e296a7fd3fa28026084697da72be7b5c2
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-11-05 14:44:58 +0300

    upd: django sql models. migration created

--- django_app/s21_tribe_tournaments/models.py
+++ django_app/s21_tribe_tournaments/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
 
 class Campuses(models.Model):
     name = models.CharField(max_length=52)
@@ -13,15 +11,6 @@ class Campuses(models.Model):
 
     def get_absolute_url(self):
         return reverse("tournaments_main", kwargs={"campus_slug": self.slug})
-
-
-class Tournaments(models.Model):
-    name = models.CharField(max_length=128)
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-
-    def __str__(self):
-        return self.name
 
 
 class Tribes(models.Model):
@@ -40,6 +29,17 @@ class Tribes(models.Model):
 
     def get_absolute_url(self):
         return reverse("campus_page", kwargs={"tribe_slug": self.slug})
+
+
+class Tournaments(models.Model):
+    name = models.CharField(max_length=128)
+    campus = models.ForeignKey(Campuses, on_delete=models.CASCADE)
+    tribe = models.ForeignKey(Tribes, on_delete=models.CASCADE)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 
 class Peers(models.Model):
